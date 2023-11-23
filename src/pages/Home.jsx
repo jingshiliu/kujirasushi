9ff825be9f354a39c3c064bc7589f107e9496059
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-23 00:50:05 -0500

    chore(Home): See Menu button opens a new tab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
 
                 <a className={'text-slate-50 border px-10 py-4 border-slate-300 hover:bg-slate-200 hover:text-slate-500 hover:bg-opacity-90 transition duration-300 ease-in-out rounded-lg shadow-md shadow-slate-100 bg-opacity-60 bg-slate-500'}
                    href={KujiraSushiMenu}
+                   target={'_blank'}
                 >
                     See Menu
                 </a>
