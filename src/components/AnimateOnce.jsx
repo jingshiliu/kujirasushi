313b6eeca6b3f212a0e93c3f41ed6a29a5f3e295
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-24 00:48:04 -0500

    chore(AnimateOnce): add prop rootMarigin

--- src/components/AnimateOnce.jsx
+++ src/components/AnimateOnce.jsx
@@ -2,9 +2,9 @@ import {useRef} from "react";
 import useAnimatedOnce from "../hook/useAnimatedOnce.js";
 
 // eslint-disable-next-line react/prop-types
-const AnimateOnce = ({className, animation, children}) => {
+const AnimateOnce = ({className, animation, children, rootMargin='-10px'}) => {
     const ref = useRef(null)
-    const hasAnimated = useAnimatedOnce(ref)
+    const hasAnimated = useAnimatedOnce(ref, rootMargin)
     if(className === undefined) className = ''
     if(animation === undefined) animation = ''
 
