b77f01e8698a77b4f62bcb364cb295ebe0a818a4
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-23 22:42:49 -0500

    refactor(Home): change Home page folder structure

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,8 +1,10 @@
 
 // eslint-disable-next-line react/prop-types
 const Background = ({image, className}) => {
+    if (className === undefined) className = '';
+
     return (
-        <div className={`absolute left-0 top-0 w-full h-full ${className}`}>
+        <div className={`-z-10 absolute left-0 top-0 w-full h-full ${className}`}>
             <img className={'object-cover w-full h-full'} src={image} alt="background image "/>
         </div>
     );
