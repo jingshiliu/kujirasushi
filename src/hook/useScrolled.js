f1dfd4ef7c5e4069342615a82504ff5c87f5610e
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-24 02:37:41 -0500

    chore(Hook): implement useScrolled

---
+++ src/hook/useScrolled.js
@@ -0,0 +1,21 @@
+import {useEffect, useState} from "react";
+
+const useScrolled = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return scrolled
+};
+
+export default useScrolled;
