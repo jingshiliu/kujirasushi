8f137cd75de9cb70234e992c9629adc5dc84174b
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-27 16:47:10 -0500

    chore(Menu): create Menu page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from "./pages/Layout.jsx";
 import Home from "./pages/Home";
 import 'animate.css/animate.min.css';
+import Menu from "./pages/Menu";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
           <Routes>
               <Route path={'/'} element={<Layout />}>
                   <Route index element={<Home/>}/>
+                  <Route path={'menu'} element={<Menu/>}/>
               </Route>
           </Routes>
       </BrowserRouter>
---
+++ src/pages/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import Nav from "../../components/Nav.jsx";
+import Footer from "../../components/Footer.jsx";
+
+const Menu = () => {
+    return (
+        <main className={"bg-black h-screen w-screen overflow-x-hidden"}>
+            <Nav/>
+            <Footer/>
+        </main>
+    );
+};
+
+export default Menu;
