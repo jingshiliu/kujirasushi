160c3676dae8ab581527f8613b47368a37471b1c
Jingshi Liu <jingshiliu07@gmail.com>
2023-12-03 12:58:40 -0500

    feat(Util): implement getImageUrl()

--- src/util.js
+++ src/util.js
@@ -4,3 +4,17 @@ export const scrollToHash = (hash) => {
         if (element) element.scrollIntoView();
     }
 };
+
+export const getNetlifyImageCDNUrl = (url, width=800, height=600, fit = 'cover') => {
+    return `/.netlify/images?url=${url}&w=${width}&h=${height}&fit=${fit}`
+}
+
+
+export const getImageUrl = (imageName, width=800, height=600, fit = 'cover', source='netlify') => {
+    // before deploy to netlify, change url to `assets/images/${imageName}`
+    const url = `src/assets/images/${imageName}`;
+    if (source === 'netlify') {
+        return getNetlifyImageCDNUrl(url, width, height, fit);
+    }
+    return url;
+}
