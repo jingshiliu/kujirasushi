f4de7d07810bd5b3d7796a522d49d09fa06f6ef2
Jingshi Liu <jingshiliu07@gmail.com>
2023-12-03 12:58:08 -0500

    chore(Nav): remove default font color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = ({className}) => {
         <header className={`lg:px-20 px-12 p-10 flex justify-between items-center fixed w-screen z-50 max-sm:h-[30px] ${className}
                             ${scrolled && !showMenu? "bg-opacity-90 bg-slate-500":""}`}>
             <Link to="/#home" className={'z-20'}>
-                <h1 className={'flex items-center text-slate-100'}>
+                <h1 className={'flex items-center'}>
                     <GiSushis className={'w-10 h-10'}/>
                     <span className={'text-lg font-mono ml-2'}>
                     Kujira
@@ -21,7 +21,7 @@ const Nav = ({className}) => {
                 </h1>
             </Link>
             <nav
-                className={'text-base flex justify-between lg:space-x-14 text-slate-300 font-light max-sm:hidden sm:space-x-8 items-center'}
+                className={'text-base flex justify-between lg:space-x-14 font-light max-sm:hidden sm:space-x-8 items-center'}
             >
                 <Link to="/#home">Home</Link>
                 <Link to="/#about">About Us</Link>
@@ -30,7 +30,7 @@ const Nav = ({className}) => {
             </nav>
 
             <button
-                className={'sm:hidden object-contain w-8 text-slate-200 z-20'}
+                className={'sm:hidden object-contain w-8 z-20'}
                 onClick={() => setShowMenu(!showMenu)}
             >
                 <RxHamburgerMenu className={'w-full h-full'} />
@@ -42,10 +42,10 @@ const Nav = ({className}) => {
                                 absolute left-0 top-0 border-b w-full space-y-5 text-center animate__animated animate__fadeInDown
                                 ${showMenu? "max-sm:bg-opacity-80 max-sm:bg-slate-500 translate-y-10 pt-20":""}`}
                 >
-                    <a className={'hover:text-slate-400'} href="#home">Home</a>
-                    <a className={'w-1/3 border-t border-slate-500 pt-4 hover:text-slate-400'} href="#about">About Us</a>
-                    <a className={'w-1/3 border-t border-slate-500 pt-4 hover:text-slate-400'} href="">Menu</a>
-                    <a className={'w-1/3 border-t border-slate-500 pt-4 hover:text-slate-400 pb-4'} href="">Contact Us</a>
+                    <a className={'hover:text-slate-400'} href="/#home">Home</a>
+                    <a className={'w-1/3 border-t border-slate-500 pt-4 hover:text-slate-400'} href="/#about">About Us</a>
+                    <a className={'w-1/3 border-t border-slate-500 pt-4 hover:text-slate-400'} href="/menu">Menu</a>
+                    <a className={'w-1/3 border-t border-slate-500 pt-4 hover:text-slate-400 pb-4'} href="/#contact">Contact Us</a>
                 </nav>
             ) : <></>}
         </header>
