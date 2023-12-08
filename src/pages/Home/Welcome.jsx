9374309270077a2a3c2fec5d4d803dc1e2eee1a3
Jingshi Liu <jingshiliu07@gmail.com>
2023-12-08 11:58:28 -0500

    feat(Home): add new button Appointment & Waitlist

--- src/pages/Home/Welcome.jsx
+++ src/pages/Home/Welcome.jsx
@@ -14,13 +14,31 @@ const Welcome = () => {
                     <h4 className={'tracking-widest -translate-y-2 text-lg'}>Pittsburgh</h4>
                 </div>
 
-                <a className={`text-slate-50 border px-10 py-4 border-slate-300 hover:bg-slate-200 hover:text-slate-500 
+                <div className={'text-center space-y-4'}>
+                    <a className={`text-slate-50 border px-10 py-4 border-slate-300 hover:bg-slate-200 hover:text-slate-500 
+                                hover:bg-opacity-90 transition duration-300 ease-in-out shadow-md shadow-slate-100 
+                                bg-opacity-60 bg-slate-500 animate__animated animate__fadeInUp block w-100`}
+                       href={'/menu'}
+                    >
+                        Menu
+                    </a>
+                    <a className={`text-slate-50 border px-10 py-4 border-slate-300 hover:bg-slate-200 hover:text-slate-500 
+                                hover:bg-opacity-90 transition duration-300 ease-in-out shadow-md shadow-slate-100 
+                                bg-opacity-60 bg-slate-500 animate__animated animate__fadeInUp block w-100`}
+                       href={'https://tables.toasttab.com/restaurants/2753653b-5a1e-4668-8f46-6af4d49e50df/findTime'}
+                       target={'_blank'}
+                    >
+                        Appointment
+                    </a>
+                    <a className={`text-slate-50 border px-10 py-4 border-slate-300 hover:bg-slate-200 hover:text-slate-500 
                                 hover:bg-opacity-90 transition duration-300 ease-in-out shadow-md shadow-slate-100 
                                 bg-opacity-60 bg-slate-500 animate__animated animate__fadeInUp block mb-10 w-100`}
-                   href={'/menu'}
-                >
-                    Menu
-                </a>
+                       href={'https://tables.toasttab.com/restaurants/2753653b-5a1e-4668-8f46-6af4d49e50df/joinWaitlist'}
+                       target={'_blank'}
+                    >
+                        Waitlist
+                    </a>
+                </div>
             </div>
             <ScrollableNotification/>
         </section>
