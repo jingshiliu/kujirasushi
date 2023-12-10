fb929436c270ecad4866fd50399f7fb108767013
Jingshi Liu <jingshiliu07@gmail.com>
2023-12-10 15:26:53 -0500

    feat(Home): add a new button Order Online and delete the Menu button

--- src/pages/Home/Welcome.jsx
+++ src/pages/Home/Welcome.jsx
@@ -18,9 +18,10 @@ const Welcome = () => {
                     <a className={`text-slate-50 border px-10 py-4 border-slate-300 hover:bg-slate-200 hover:text-slate-500 
                                 hover:bg-opacity-90 transition duration-300 ease-in-out shadow-md shadow-slate-100 
                                 bg-opacity-60 bg-slate-500 animate__animated animate__fadeInUp block w-100`}
-                       href={'/menu'}
+                       href={'https://order.toasttab.com/online/captain-sushi-shadyside-736-bellefonte-st'}
+                       target={'_blank'}
                     >
-                        Menu
+                        Order Online
                     </a>
                     <a className={`text-slate-50 border px-10 py-4 border-slate-300 hover:bg-slate-200 hover:text-slate-500 
                                 hover:bg-opacity-90 transition duration-300 ease-in-out shadow-md shadow-slate-100 
