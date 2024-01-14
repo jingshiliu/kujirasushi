2ae19912730ccf0b3a012574858082ee3eb032a3
Jingshi Liu <jingshiliu07@gmail.com>
2024-01-13 20:47:12 -0500

    feat(ScrollableNotification): redesign

--- src/components/ScrollableNotification.jsx
+++ src/components/ScrollableNotification.jsx
@@ -3,10 +3,7 @@ import { IoIosArrowDown } from "react-icons/io";
 const ScrollableNotification = () => {
     return (
         <div className={`absolute bottom-0`}>
-            <div className={`flex flex-col items-center space-y-2 text-slate-300 text-lg animate-pulse`}>
-                <span>Scroll to see more</span>
-                <IoIosArrowDown />
-                <IoIosArrowDown />
+            <div className={`flex flex-col items-center space-y-2 text-slate-300 text-lg animate-bounce`}>
                 <IoIosArrowDown />
             </div>
         </div>
